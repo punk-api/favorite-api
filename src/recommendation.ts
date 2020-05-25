4b6ba8ac8b080b84685187d033acd936f45509da
Larry Boltovskoi <larry.bolt@gmail.com>
2020-05-25 20:15:54 +0100

    Add most popular calculation

--- src/recommendation.ts
+++ src/recommendation.ts
@@ -1,16 +1,22 @@
-const recommend = require('collaborative-filter');
+const recommend = require("collaborative-filter");
 import flatten = require("lodash.flatten");
+import orderby = require("lodash.orderby");
 import { catchError, error, success } from "./lambda-helpers";
 import { getAllFavoritesByUser } from "./ddb-favorites";
-import { allItems } from './all-items';
+import { allItems } from "./all-items";
 
-export function calculate(groups: {[groupName: string]: string[]}, forUser: string) {
-    const users = Object.keys(groups);
-    const allItems = flatten(Object.values(groups));
-    const items = allItems.filter((item, index, all) => all.indexOf(item) === index);
-    const ratings = users.map(user => items.map(item => groups[user].includes(item) ? 1 : 0))
-    const results: number[] = recommend.cFilter(ratings, users.indexOf(forUser));
-    return results.map(result => items[result]);
+export function calculate(
+  groups: { [groupName: string]: string[] },
+  forUser: string
+) {
+  const users = Object.keys(groups);
+  const all = flatten(Object.values(groups));
+  const items = all.filter((item, index, all) => all.indexOf(item) === index);
+  const ratings = users.map((user) =>
+    items.map((item) => (groups[user].includes(item) ? 1 : 0))
+  );
+  const results: number[] = recommend.cFilter(ratings, users.indexOf(forUser));
+  return results.map((result) => items[result]);
 }
 
 export async function getRecommendations(
@@ -21,8 +27,37 @@ export async function getRecommendations(
     if (!event.pathParameters?.username) {
       return error("Specify username", 403);
     }
-    const recommendations = calculate(await getAllFavoritesByUser(), event.pathParameters.username)
-    const recommendedItems = recommendations.map(recommendation => allItems.find(item => item.id.toString() === recommendation));
+    const username = event.pathParameters.username;
+    const groups = await getAllFavoritesByUser();
+    if (!Object.keys(groups).includes(username)) {
+      return success([]);
+    }
+    const recommendations = calculate(groups, username);
+    const recommendedItems = recommendations.map((recommendation) =>
+      allItems.find((item) => item.id.toString() === recommendation)
+    );
     return success(recommendedItems);
+  });
+}
+
+export async function getMostPopular(
+  event: AWSLambda.APIGatewayEvent,
+  context: AWSLambda.Context
+): Promise<AWSLambda.APIGatewayProxyResult> {
+  return catchError(async () => {
+    const groups = await getAllFavoritesByUser();
+    const all = flatten(Object.values(groups));
+    const items = all.filter((item, index, all) => all.indexOf(item) === index);
+    const itemsWithCount = items.map((item) => ({
+      id: item,
+      count: all.filter((i) => i === item).length,
+    }));
+    const itemsWithCountOrdered = orderby(itemsWithCount, "count", "desc");
+    const first5 = itemsWithCount.slice(0, 5);
+    const mostPopularItems = first5.map((item) => ({
+      ...allItems.find((i) => i.id.toString() === item.id),
+      favoriteCount: item.count,
+    }));
+    return success(orderby(mostPopularItems, "favoriteCount", "desc"));
   });
 }
