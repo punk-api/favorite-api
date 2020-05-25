9601392f6d3d78c9e71672508cddc97d9549f4f0
Larry Boltovskoi <larry.bolt@gmail.com>
2020-05-25 18:39:13 +0100

    Bug due to missing types, cFilter instead of CFilter

--- src/recommendation.ts
+++ src/recommendation.ts
@@ -1,4 +1,4 @@
-const recommend =  require('collaborative-filter');
+const recommend = require('collaborative-filter');
 import flatten = require("lodash.flatten");
 
 export function calculate(groups: {[groupName: string]: string[]}, forUser: string) {
@@ -6,6 +6,6 @@ export function calculate(groups: {[groupName: string]: string[]}, forUser: stri
     const allItems = flatten(Object.values(groups));
     const items = allItems.filter((item, index, all) => all.indexOf(item) === index);
     const ratings = users.map(user => items.map(item => groups[user].includes(item) ? 1 : 0))
-    const results: number[] = recommend.CFilter(ratings, users.indexOf(forUser));
+    const results: number[] = recommend.cFilter(ratings, users.indexOf(forUser));
     return results.map(result => items[result]);
 }
